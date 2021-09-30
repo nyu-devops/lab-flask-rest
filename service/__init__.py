b4aefc244283aff2e81d3c7f05a142f1eeda5520
John Rofrano <johnnyroy@johnrofrano.com>
2021-09-30 14:00:43 +0000

    Fixed INFO not being logged

--- service/__init__.py
+++ service/__init__.py
@@ -31,18 +31,16 @@ app.config.from_object("config")
 from service import routes, models, error_handlers
 
 # Set up logging for production
-if __name__ != "__main__":
-    gunicorn_logger = logging.getLogger("gunicorn.error")
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
-    app.logger.propagate = False
-    # Make all log formats consistent
-    formatter = logging.Formatter(
-        "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
-    )
-    for handler in app.logger.handlers:
-        handler.setFormatter(formatter)
-    app.logger.info("Logging handler established")
+print(f"Setting log level to {logging.INFO}")
+app.logger.setLevel(logging.INFO)
+app.logger.propagate = False
+# Make all log formats consistent
+formatter = logging.Formatter(
+    "[%(asctime)s] [%(levelname)s] [%(module)s] %(message)s", "%Y-%m-%d %H:%M:%S %z"
+)
+for handler in app.logger.handlers:
+    handler.setFormatter(formatter)
+app.logger.info("Logging handler established")
 
 app.logger.info(70 * "*")
 app.logger.info("  P E T   S E R V I C E   R U N N I N G  ".center(70, "*"))
